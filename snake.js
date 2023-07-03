1fcd33e506a522f72c9f0def0e5ec40f355da0ed
Xhoi Kabashi <100515180+Xhoo1@users.noreply.github.com>
2023-07-03 23:04:09 +0200

    Update snake.js

--- snake.js
+++ snake.js
@@ -89,7 +89,7 @@ function update() {
     snakeY > rows * blockSize
   ) {
     gameOver = true;
-    document.querySelector("h1").innerHTML = "Emo ti Karo!";
+    document.querySelector("h1").innerHTML = "Xhero Budallai!";
     document.querySelector("h1").style.color = "red";
   }
 
